b152d544e290112d3308027b03fa11fd36ec2bf0
David Sferruzza <david.sferruzza@gmail.com>
2020-05-19 00:20:41 +0200

    Fix implementation

--- srcs/pressure_controller.cpp
+++ srcs/pressure_controller.cpp
@@ -876,7 +876,4 @@ PressureController::pidPatient(int32_t targetPressure, int32_t currentPressure, 
     return patientAperture;
 }
 
-
-void PressureController::resetBlowerDefaultSpeed(){
-  blower.runSpeed(DEFAULT_BLOWER_SPEED);
-}
+void PressureController::resetBlowerDefaultSpeed() { m_blower->runSpeed(DEFAULT_BLOWER_SPEED); }
