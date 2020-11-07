0d8b05c9f8c99d60a3ee1f1b489403b122383dc1
pi-r-p <pierre.papin@senx.io>
2020-11-07 22:57:59 +0100

    added a cpu load estimation

---
+++ includes/cpuLoad.h
@@ -0,0 +1,16 @@
+/******************************************************************************
+ * @author Makers For Life
+ * @copyright Copyright (c) 2020 Makers For Life
+ * @file mass_flow_meter.h
+ * @brief Mass flow meter management
+ *****************************************************************************/
+
+#pragma once
+
+/**
+ * CPU load in percent. Only available in production software (MODE_PROD)
+ * 
+ * @note If this raise to 100%, there is some blocking code somewhere.
+ */
+void readCpuLoadPercent(void);
+
