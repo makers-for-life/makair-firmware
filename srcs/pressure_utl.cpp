d83143ff0667b467e5e9a71cfbaeb80dfb39e560
pi-r-p <pierre.papin@senx.io>
2020-11-24 23:16:41 +0100

    use ADC with a 12 bits resolution

--- srcs/pressure_utl.cpp
+++ srcs/pressure_utl.cpp
@@ -17,20 +17,33 @@ static int32_t filteredRawPressure = 0;
 
 static const int32_t RAW_PRESSURE_FILTER_DIVIDER = 5;
 
-// Constants used in original conversion formula:
-//      RATIO_VOLTAGE_DIVIDER = 0.8192
-//      V_SUPPLY = 5.08
-//      KPA_MMH2O = 101.97162129779
+// From the datasheet:
+// Transfer Function (kPa): Vout = VS×(0.09×P + 0.04)
+// VS is 5.05 volt typical on our boards
+// Transfer function for P in mmH2O, with 1 kPa = 101.97162129779 mmH2O
+// Vout = VS×(0.09×(P/101.9716) + 0.04)
+// P = (101.9716/0.09)×(Vout/VS-0.04)
+// P(mmH20) = 1133(Vout/VS) - 45
+// The offset really depend on the sensor itself with a great variation
+// There must be a calibration sequence to set the zero.
+//
+// VS is 5.05V.
+//
+// There is a voltage divider in between Vout and Vadc
+// Vadc = 68/83 Vout
+// 
+// With current ADC (12 bits), and mean Vref=3.348V:
+// Vadc = 3.348 * RawAdc/4096 = 0,000817382 * RawAdc
+// 
+// Vout = 83/68 * 0,000817382 * RawAdc
+// Vout = 0,000997686 * RawAdc
+// 
+// Put it together:
+// P(mmH20) = 1133((0,000997686 * RawAdc)/5.05) - 45
+// P(mmH20) = 0,223837466 * RawAdc - 45
 
-// RAW_PRESSURE_TO_MMH20_CONSTANT is computed from original
-// formula as:
-//      0.04 / 0.09 * KPA_MMH2O
 static const int16_t RAW_PRESSURE_TO_MMH20_CONSTANT = 45;
-
-// RAW_PRESSURE_TO_MMH20_NUM / RAW_PRESSURE_TO_MMH20_DEN is computed
-// from original formula as:
-//      3.3 / 1024.0 / RATIO_VOLTAGE_DIVIDER / V_SUPPLY / 0.09 * KPA_MMH2O
-static const int32_t RAW_PRESSURE_TO_MMH20_NUM = 8774;
+static const int32_t RAW_PRESSURE_TO_MMH20_NUM = 2238;
 static const int32_t RAW_PRESSURE_TO_MMH20_DEN = 10000;
 
 // FUNCTIONS ==================================================================
