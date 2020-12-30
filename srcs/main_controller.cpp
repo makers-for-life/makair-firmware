f466d4ac0e8c73bf9b45003c8a8950f2fe636a1e
Gabriel Moneyron <gabriel.moneyron@yahoo.fr>
2020-12-30 10:34:32 +0100

    use expi flow meter

--- srcs/main_controller.cpp
+++ srcs/main_controller.cpp
@@ -605,9 +605,7 @@ void MainController::sendStopMessageToUi() {
         m_lowTidalVolumeAlarmTresholdNextCommand, m_highTidalVolumeAlarmTresholdNextCommand,
         m_plateauDurationNextCommand, 0u,
         static_cast<uint8_t>(m_targetInspiratoryFlowNextCommand / 1000),
-        m_inspiratoryDurationNextCommand,
-        getBatteryLevelX10(),
-        alarmController.triggeredAlarms());
+        m_inspiratoryDurationNextCommand, getBatteryLevelX10(), alarmController.triggeredAlarms());
 }
 
 void MainController::stop(uint32_t p_currentMillis) {
@@ -657,8 +655,8 @@ void MainController::sendMachineState() {
         m_lowTidalVolumeAlarmTresholdNextCommand, m_highTidalVolumeAlarmTresholdNextCommand,
         m_plateauDurationNextCommand, 0u,
         static_cast<uint8_t>(m_targetInspiratoryFlowNextCommand / 1000),
-        m_inspiratoryDurationNextCommand, m_ticksPerInhalation*MAIN_CONTROLLER_COMPUTE_PERIOD_MS,
-        getBatteryLevelX10());//todo check tick per inhalation accuracy
+        m_inspiratoryDurationNextCommand, m_ticksPerInhalation * MAIN_CONTROLLER_COMPUTE_PERIOD_MS,
+        getBatteryLevelX10());  // todo check tick per inhalation accuracy
 }
 
 void MainController::onVentilationModeSet(uint16_t p_ventilationControllerMode) {
