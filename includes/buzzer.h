77f8729c9c89601c40cdd45b11414795ae3225d9
Gabriel Moneyron <gabriel.moneyron@yahoo.fr>
2023-07-26 10:33:37 +0200

    buzzer

--- includes/buzzer.h
+++ includes/buzzer.h
@@ -8,7 +8,7 @@
 #pragma once
 
 /// Watchdog timeout in microseconds
-#define WATCHDOG_TIMEOUT 1000000
+#define WATCHDOG_TIMEOUT 5000000
 
 /// Hardware Timer to use for the buzzer
 #define BUZZER_TIMER TIM5  // TIM5 is a 32 bits timer
